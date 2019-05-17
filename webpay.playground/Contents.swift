a3abac9d04c5e377774182782c0303a3bddb4ebc
frankfaustino <nawzyah@gmail.com>
2019-05-16 23:17:16 -0700

    Add webpay.playground

---
+++ webpay.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+
+// Configuration setup
+let merchantId = ""
+let orderId = ""
+let access_token = ""
+
+// Clover's Sandbox environment
+let targetEnv = "https://sandbox.dev.clover.com"
+let v2MerchantPath = "/v2/merchant/"
+
+// Example values
+let amount = 1000
+let tipAmount = 0
+let taxAmount = 0
+let cardNumber = "6011361000006668"
+let expMonth = 12
+let expYear = 2018
+let cvv = 123
+let zipCode = 94085
