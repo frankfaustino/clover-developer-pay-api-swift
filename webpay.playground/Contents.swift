25277b561ddb249b13957cf7099fa98a0f95a34c
frankfaustino <nawzyah@gmail.com>
2019-05-21 20:43:51 -0700

    Add configuration check

--- webpay.playground/Contents.swift
+++ webpay.playground/Contents.swift
@@ -2,9 +2,9 @@ import Cocoa
 
 
 // Configuration setup
+let accessToken = ""
 let merchantId = ""
 let orderId = ""
-let access_token = ""
 
 // Clover's Sandbox environment
 let targetEnv = "https://sandbox.dev.clover.com"
@@ -19,3 +19,32 @@ let expMonth = 12
 let expYear = 2018
 let cvv = 123
 let zipCode = 94085
+
+
+enum TestError: Error {
+    case emptyAccesToken
+    case emptyMerchantId
+    case emptyOrderId
+}
+
+func configCheck() throws {
+    if accessToken.isEmpty {
+        throw TestError.emptyAccesToken
+    }
+    if merchantId.isEmpty {
+        throw TestError.emptyMerchantId
+    }
+    if orderId.isEmpty {
+        throw TestError.emptyOrderId
+    }
+}
+
+do {
+    try configCheck()
+} catch TestError.emptyAccesToken {
+    print("Remember to set your access_token with PROCESS_CARDS permission on line 13. For help creating an access_token, read https://docs.clover.com/clover-platform/docs/using-oauth-20")
+} catch TestError.emptyMerchantId {
+    print("Set your merchant ID, which can be found in your merchant dashboard: https://sandbox.dev.clover.com/developers")
+} catch TestError.emptyOrderId {
+    print("For help creating an order, read https://docs.clover.com/clover-platform/docs/working-with-orders")
+}
