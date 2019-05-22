78203cbf47e690a76de970345113d43e59bc3dc9
Frank Faustino <nawzyah@gmail.com>
2019-05-21 20:48:25 -0700

    Add /pay/key URLRequest for encryption information

--- webpay.playground/Contents.swift
+++ webpay.playground/Contents.swift
@@ -20,6 +20,9 @@ let expYear = 2018
 let cvv = 123
 let zipCode = 94085
 
+var exponent: String? = nil
+var modulus: String? = nil
+var prefix: String? = nil
 
 enum TestError: Error {
     case emptyAccesToken
@@ -48,3 +51,36 @@ do {
 } catch TestError.emptyOrderId {
     print("For help creating an order, read https://docs.clover.com/clover-platform/docs/working-with-orders")
 }
+
+// GET /v2/merchant/{mId}/pay/key for the encryption information needed for the pay endpoint
+var url = targetEnv + v2MerchantPath + merchantId + "/pay/key"
+print("Requesting GET " + url)
+print("Bearer " + accessToken)
+
+var request = URLRequest(url: URL(string: url)!)
+request.httpMethod = "GET"
+request.addValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+
+URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+    let httpResponse = response as! HTTPURLResponse
+
+    if httpResponse.statusCode == 200 {
+        var jsonDictionary: NSDictionary? = nil
+
+        do {
+            jsonDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+        } catch {
+            print("JSON Serialization error")
+        }
+
+        if jsonDictionary != nil {
+            exponent = jsonDictionary?.object(forKey: "exponent") as? String
+            modulus = jsonDictionary?.object(forKey: "modulus") as? String
+            prefix = jsonDictionary?.object(forKey: "prefix") as? String
+            print(exponent!, modulus!, prefix!)
+        }
+    } else {
+        print(httpResponse.statusCode)
+        print("Read 'Troubleshooting common Clover REST API error codes' at https://medium.com/clover-platform-blog/troubleshooting-common-clover-rest-api-error-codes-9aaa8885373")
+    }
+}).resume()
